3e387d998c6abde8e85762e399381b5186f0f9eb
nguyen <volen@MYpc.(none)>
2022-12-17 00:17:12 +0700

    nguyen

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <DHT.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h> 
+#include <WiFiManager.h>
 #include <EEPROM.h>
 #include <Arduino.h>
 #include <stdio.h>
@@ -18,25 +18,24 @@
 
 #define DHTPIN 10 /*chân out duoc noi với GPIO 10 */
 #define DHTTYPE DHT11
-#define bt D8
+#define bt 9
 /* dinh nghia nut nhan va den */
 /* nut nhan ao ( virtualpin) */
-#define Vir_Pin1   V1
-#define Vir_Pin2   V2
-#define Vir_Pin3   V3
-#define Vir_Pin4   V4
-
+#define Vir_Pin1 V1
+#define Vir_Pin2 V2
+#define Vir_Pin3 V3
+#define Vir_Pin4 V4
 
 // nut nhan
-#define BTN1 14
-#define BTN2 12
-#define BTN3 13
-#define BTN4 15
+#define BTN1 5
+#define BTN2 4
+#define BTN3 0
+#define BTN4 2
 // relay va led
-#define LED_1 16
-#define LED_2 5
-#define LED_3 4
-#define LED_4 0
+#define LED_1 14
+#define LED_2 12
+#define LED_3 13
+#define LED_4 15
 /* ------- */
 
 int relaystate1 = 0;
@@ -58,35 +57,42 @@ WiFiManager wifiManager;
 BLYNK_WRITE(Vir_Pin1)
 {
   relaystate1 = param.asInt(); // gán giá chị cho biến V1
-  digitalWrite(LED_1,!relaystate1);
+  digitalWrite(LED_1, !relaystate1);
   Serial.print("Relay 1 = ");
-  if(relaystate1 == 0) Serial.println("OFF");
-  else Serial.println("ON");
+  if (relaystate1 == 0)
+    Serial.println("OFF");
+  else
+    Serial.println("ON");
 }
 BLYNK_WRITE(Vir_Pin2)
 {
   relaystate2 = param.asInt(); // gắn giá chị cho V2
-  digitalWrite(LED_2,!relaystate2);
+  digitalWrite(LED_2, !relaystate2);
   Serial.print("Relay 2 ");
-  if(relaystate2 == 0) Serial.println("OFF");
-  else Serial.println("ON");
+  if (relaystate2 == 0)
+    Serial.println("OFF");
+  else
+    Serial.println("ON");
 }
 BLYNK_WRITE(Vir_Pin3)
 {
   relaystate3 = param.asInt();
   digitalWrite(LED_3, !relaystate3);
   Serial.print("Relay 3 ");
-  if(relaystate3 == 0) Serial.println("OFF");
-  else Serial.println("on");
+  if (relaystate3 == 0)
+    Serial.println("OFF");
+  else
+    Serial.println("on");
 }
 BLYNK_WRITE(Vir_Pin4)
 {
   relaystate4 = param.asInt(); // gán giá chị cho biến V1
   digitalWrite(LED_4, !relaystate4);
   Serial.print("Relay 4 ");
-  if(relaystate4 == 0) Serial.println("OFF");
-  else Serial.println("ON");
-
+  if (relaystate4 == 0)
+    Serial.println("OFF");
+  else
+    Serial.println("ON");
 }
 
 /* cài đặt  thông số */
@@ -106,7 +112,7 @@ void setup()
   pinMode(bt, INPUT); // bt wire D8 and 3.3V
   EEPROM.begin(512);
   Serial.begin(115200);
-  //Blynk.begin(auth, SSID, pass);
+  // Blynk.begin(auth, SSID, pass);
   Serial.println("DHT11__test!");
   dht.begin(); // Bắt đầu đọc dữ liệu
   delay(3000);
@@ -115,8 +121,8 @@ void setup()
     wifiManager.resetSettings();
     delay(1000);
   }
-  //  wifiManager.resetSettings();    
-  //Uncomment this to wipe WiFi settings from EEPROM on boot.  Comment out and recompile/upload after 1 boot cycle.
+  //  wifiManager.resetSettings();
+  // Uncomment this to wipe WiFi settings from EEPROM on boot.  Comment out and recompile/upload after 1 boot cycle.
   wifiManager.autoConnect("ESP_8266");
   // if you get here you have connected to the WiFi
   Serial.println("connected...ok :)");
@@ -211,7 +217,7 @@ void Control_relay(int relay)
       Serial.println("OFF");
     else
       Serial.println("ON");
-  } 
+  }
 }
 void listen_push_buttons()
 {
@@ -259,5 +265,4 @@ void loop()
   Blynk.run();
   timer.run();
   listen_push_buttons();
-  
 }
